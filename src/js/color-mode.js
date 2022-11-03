f1f51fdc19c3787f79b8a2a14e9f546549a58da7
AzEdownload <contact@aze.email>
2022-11-03 23:45:53 +0100

    Added a github icon.

--- src/js/color-mode.js
+++ src/js/color-mode.js
@@ -1,15 +1,18 @@
 var icon = document.getElementById("icon")
+var github = document.getElementById("github")
 var link = document.querySelector("link[rel~='icon']");
 
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")) {
+        github.src = "assets/images/github-light.png"
         icon.src ="assets/images/sun.png"
         link = document.createElement('link');
         link.rel = 'icon';
         document.getElementsByTagName('head')[0].appendChild(link);
         link.href = 'assets/images/icon-dark.png'
     } else {
+        github.src = "assets/images/github-dark.png"
         icon.src = "assets/images/moon.png"
         link = document.createElement('link');
         link.rel = 'icon';
@@ -17,3 +20,4 @@ icon.onclick = function() {
         link.href = 'assets/images/icon-light.png'
     }
 }
+
